5d1d2b63f711efe72297e9d5df8d8d79e0fd82cb
Ajil Raju <ajilraju01@gmail.com>
2019-09-24 06:28:53 +0530

    wip/ not stable

---
+++ notes.py
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+import argparse
+import sys
+
+def err_exit(msg):
+	sys.stderr.write(msg + '\n')
+	sys.exit()
+
+def read_notes():
+        # To constantly read from the stdin and send to the files
+        # until the # char occurred.  
+        done = False	
+        print('Enter the your notes, (Use # to quit and save from notes)')
+
+        while not done:
+        	read_line = sys.stdin.readline()
+	        if read_line != '#\n':
+	                write_to_file(read_line)
+	        else:
+	                done = True
+	                
+def write_to_file(mesg):
+        # To open and write the actual notes data to files 
+        filename = './notesfiles.txt'
+        try:
+                file = open(filename, 'w')
+                file.write(mesg + '\n')
+        except IOError:
+                print("Can't write to file")
+                sys.exit(1)
+
+def main():
+
+	parser = argparse.ArgumentParser(description='Tool for adding insta notes')
+	parser.add_argument('-t',
+                                '--title',
+                                action='store',
+				required='True',
+				help='set title to note.')
+
+	args = parser.parse_args()
+
+	if args.title:
+		NOTE_TITLE = args.title
+		read_notes()
+		
+		
+if __name__ == '__main__':
+	main()
