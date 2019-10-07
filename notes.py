75bda74ed42d6732d7532f96ffeabbd27e4c1534
Ajil Raju <ajilraju01@gmail.com>
2019-10-07 07:00:07 +0530

    clean code && added new opts args

--- notes.py
+++ notes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import argparse
 import sys
 import time
@@ -8,14 +9,14 @@ def read_notes():
 	# To constantly read from the stdin and send to the files
     # until the # char occurred.  
 
-    line_holder = []
+    line_holder = ""
     done = False	
-    print('Put your note here, (Use # to quit and save from notes)')
+    print('Put your todo here, (Use # to quit and save from qnotes)')
 
     while not done:
         read_line = sys.stdin.readline()
         if read_line != '#\n':
-            line_holder.append(read_line)
+            line_holder += read_line
         else:
             done = True
     return line_holder
@@ -26,7 +27,7 @@ def write_to_file(notes, title):
     
     try:
         file = open(filename, 'a')
-        file.write(str({'time': time.time(), 'title': title, 'note': str(notes)}))
+        file.write(str({'time': time.time(), 'title': title, 'todo': str(notes)}))
         file.write("\n")
     except IOErro:
         print("Can't write to file")
@@ -34,17 +35,25 @@ def write_to_file(notes, title):
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Tool for adding insta notes')
+    parser = argparse.ArgumentParser(description='Tool for adding quick todo')
     parser.add_argument('-t','--title',
                                       action='store',
-                                      required='True',
-                                      help='Set titles for notes.')
-
+                                      help='set title for the todo\'s')
+    parser.add_argument('-l', '--list',                   
+                                      action='store_true',
+                                      help='view the todo\'s')
     args = parser.parse_args()
 
+    
     if args.title:
         notes_title = args.title
+    
+    if args.list:
+        # Here is showing the stored todo's
+		sys.exit(0)
         
+    # reading todo from the stdin and once the todo is grabed 
+    # the content is write to the file
     notes = read_notes()
     if notes:
 	    write_to_file(notes, notes_title)
@@ -52,4 +61,4 @@ def main():
 	    sys.exit(1)
 	
 if __name__ == '__main__':
-   main()
+    main()
